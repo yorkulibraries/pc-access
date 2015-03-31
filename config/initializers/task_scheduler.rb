59f077b0ed0a9a650f804808a49e81050abf25f2
Tuan Nguyen <amtuannguyen@gmail.com>
2015-03-31 15:19:02 -0400

    check last_ping time and flag unused computers every minute

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,12 +1,16 @@
 require 'rubygems'
 require 'rufus/scheduler'
  
-## to start scheduler
 scheduler = Rufus::Scheduler.new
- 
-## It will print message every i minute
+# run every minute
 scheduler.every("1m") do
-  Computer.where("current_username IS NOT NULL AND last_ping <= ?", 15.minutes.ago).each do |computer|
-    Rails.logger.info "#{computer.last_ping}"
+  # get all computers that have not "ping'ed" the server within the last 16 minutes
+  # and flag them as available
+  Computer.where("current_username IS NOT NULL AND last_ping < ?", 16.minutes.ago).each do |computer|
+    Rails.logger.debug "#{computer.ip} last_ping=#{computer.last_ping} - not-in-used"
+    computer.last_ping = nil
+    computer.last_username = computer.current_username
+    computer.current_username = nil
+    computer.save
   end
 end
