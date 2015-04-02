530ed09326df02dc243a795c8dc32fae1c4fd083
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-02 15:00:10 -0400

    disable scheduler for now

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,7 +6,7 @@ scheduler = Rufus::Scheduler.new
 # run every minute
 scheduler.every("1m") do
   Computer.in_use.last_ping_more_than_x_time_ago(15.minutes).each do |computer|
-    computer.logoff
-    computer.save
+    #computer.logoff
+    #computer.save
   end
 end
