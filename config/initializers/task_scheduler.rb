b43e2562cf6998fc0c632e8210204fc8ba0d6a9c
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-02 14:54:11 -0400

    update task scheduler

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,8 +5,7 @@ scheduler = Rufus::Scheduler.new
 
 # run every minute
 scheduler.every("1m") do
-  Computer.in_use.last_ping_more_than_x_time_ago(1.minutes).each do |computer|
-    Rails.logger.debug "#{computer.ip} last_ping=#{computer.last_ping}"
+  Computer.in_use.last_ping_more_than_x_time_ago(15.minutes).each do |computer|
     computer.logoff
     computer.save
   end
