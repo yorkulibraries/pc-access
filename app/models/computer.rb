fa12630586d139792f85f4f407546a07468a1596
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-06 15:08:24 -0400

    last_ping_more_than_x_time_ago exclude NULL last_ping

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -3,7 +3,7 @@ class Computer < ActiveRecord::Base
   
   scope :in_use, -> { where("current_username IS NOT NULL AND is_powered_off = ?", false) }
   scope :not_in_use, -> { where("current_username IS NULL OR is_powered_off = ?", true) }
-  scope :last_ping_more_than_x_time_ago, ->(time) { where("last_ping < ?", time.ago) }
+  scope :last_ping_more_than_x_time_ago, ->(time) { where("last_ping IS NOT NULL AND last_ping < ?", time.ago) }
   scope :powered_off, -> { where("is_powered_off = ?", true) }
   
   def logon(username)
