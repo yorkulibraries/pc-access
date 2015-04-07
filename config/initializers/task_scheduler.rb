3d0da840090c46b97ddb496643f3087a06be3720
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-07 10:54:35 -0400

    add debug message whenever task scheduler runs

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,6 +6,8 @@ scheduler = Rufus::Scheduler.new
 
 # run every minute
 scheduler.every("1m") do
+  Rails.logger.debug("Task scheduler running at: #{Time.now}")
+  
   Computer.in_use.last_ping_more_than_x_time_ago(15.minutes).each do |computer|
     #computer.logoff
     #computer.save
