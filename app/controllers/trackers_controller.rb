65e900ea632bfb73b7c59a9da8ee466bbd4d76df
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-08 14:25:27 -0400

    added startup method to trackers

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -28,4 +28,17 @@ class TrackersController < ApplicationController
     end
   end
   
+  # /trackers/startup
+  def startup
+    @computer = Computer.find_by_ip(request.remote_ip)
+    if @computer.nil?
+      @computer = Computer.new
+      @computer.ip = request.remote_ip
+    end
+    @computer.is_powered_off = false
+    @computer.save
+    respond_to do |format|
+      format.all { render :nothing => true }
+    end
+  end
 end
