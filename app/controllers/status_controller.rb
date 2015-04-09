3a3a2970e0e23324d67906aa80be3eda397be5c9
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-09 16:50:21 -0400

    status html

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,6 +3,7 @@ class StatusController < ApplicationController
   def index
     @in_use = Computer.in_use
     @not_in_use = Computer.not_in_use
+    @ping_timed_out = Computer.ping_timed_out
     status = { in_use_count: @in_use.size, in_use: @in_use, not_in_use_count: @not_in_use.size, not_in_use: @not_in_use }
     respond_to do |format|
       format.html
