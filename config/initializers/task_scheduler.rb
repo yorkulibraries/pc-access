ca61fc495cd37e461d96d92ffcb2c266c4cbfd66
Tuan Nguyen <amtuannguyen@gmail.com>
2015-04-09 16:09:06 -0400

    scheduled task update

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,6 +5,11 @@ scheduler = Rufus::Scheduler.new
 
 # run every minute
 scheduler.every("1m") do
-  Rails.logger.debug("Task scheduler running at: #{Time.now}")
-
+  Rails.logger.info("Task scheduler running at: #{Time.now}")
+  
+  Computer.in_use.keep_alive_timed_out.each do |pc|
+    Rails.logger.info("#{pc.ip} keep_alive_timed_out => logging off")
+    pc.logoff
+    pc.save
+  end
 end
