f0740308c73ba8a023df2de4d454354e5d3eeab8
Taras Danylak <tdanylak@yorku.ca>
2015-09-24 12:34:30 -0400

    Updated how layout works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def app_version
     App::Version.new.version
   end
+
+  def app_name
+    "PC Access"
+  end
 end
--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -8,4 +8,28 @@ module LayoutHelper
   def show_title?
     @show_title
   end
+
+
+  def page_header(&block)
+    content_for(:page_header) do
+      yield block
+    end
+  end
+
+  def sidebar(&block)
+     content_for (:sidebar) do
+         yield block
+     end
+  end
+
+  def link_active_if?(c, a = nil)
+    if a != nil
+      return "active" if controller.controller_name == c && controller.action_name == a
+    else
+      return "active" if controller.controller_name == c || c.include?(controller.controller_name)
+    end
+
+    return ""
+  end
+  
 end
