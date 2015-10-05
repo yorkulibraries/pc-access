f5134b26a3c1ad5bc88a115dc78e4572a93d0ec5
Taras Danylak <tdanylak@yorku.ca>
2015-10-05 11:10:39 -0400

    added a task to attach computers to location every 10min for now

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,6 +6,15 @@ scheduler = Rufus::Scheduler.new
 # run every minute
 scheduler.every("1m") do
   Rails.logger.info("Task scheduler running at: #{Time.now}")
-  
+
   Computer.free_inactive_computers
+end
+
+
+scheduler.every("10m") do
+  Rails.logger.info("Task scheduler running at: #{Time.now}")
+
+  Computer.all.each do |c|
+    c.attach_to_location
+  end
 end
