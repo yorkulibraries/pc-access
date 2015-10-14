3f77777d3b05dcd0e604f4222157dfce9310ceb3
Taras Danylak <tdanylak@yorku.ca>
2015-10-14 13:19:54 -0400

    Added mini_magick gem and working on uploader

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem "font-awesome-rails", '4.4.0'
 gem 'rufus-scheduler', "3.0.9"
 gem 'ipaddress', "0.8.0"
 gem 'carrierwave', "0.10.0"
+gem "mini_magick", "4.3.6"
 
 
 # DEPLOYMENT
--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,9 @@
 class Area < ActiveRecord::Base
   # FIELDS: name, department, specia_access, map, notes, floor_id, location_id
 
+  ## Uploader
+  mount_uploader :map, MapUploader
+
   ## RELATIONS
   belongs_to :location
   belongs_to :floor
@@ -21,7 +24,7 @@ class Area < ActiveRecord::Base
 
     # detach computers from area first
     Computer.detach_from_area(self[:id])
-    
+
     sanitized_ips = ip_list.lines.collect { |ip| ip.rstrip }
     new_computers = Computer.where("ip in (?)", sanitized_ips)
 
