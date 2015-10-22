be1b5f5c363bfa7dac6364ae2186d4497fcb9819
Taras Danylak <tdanylak@yorku.ca>
2015-10-22 13:22:14 -0400

    Fixed images not being shown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,15 +9,21 @@ module ApplicationHelper
   end
 
   def format(field, simple_format = false)
-      # If field is blank, print out blank message
-      if field.blank?
-        content_tag(:span, "Not filled in...", class: "empty-field")
+    # If field is blank, print out blank message
+    if field.blank?
+      content_tag(:span, "Not filled in...", class: "empty-field")
+    else
+      if field.is_a? Date
+        field.strftime("%B %d, %Y")
       else
-        if field.is_a? Date
-          field.strftime("%B %d, %Y")
-        else
-          simple_format ? simple_format(field) : field
-        end
+        simple_format ? simple_format(field) : field
       end
     end
+  end
+
+  def path_to_url(path)
+    separator = path.start_with?("/") ? "" : "/"
+    "http://#{request.host_with_port}#{separator}#{path}"
+  end
+
 end
