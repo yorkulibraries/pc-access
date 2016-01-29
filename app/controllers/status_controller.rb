4641bd2f8085c53e61dd4891ece7b5be47a10afe
Taras Danylak <tdanylak@yorku.ca>
2016-01-29 12:05:45 -0500

    Changed default page

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,31 +1,12 @@
 class StatusController < ApplicationController
 
   def index
-    @in_use = Computer.in_use
-    @not_in_use = Computer.not_in_use
-    @not_pinging = Computer.not_pinging
-    @pinging = Computer.pinging
-    @staying_active = Computer.staying_active
-    @not_staying_active = Computer.not_staying_active
-    @never_ping = Computer.never_ping
-    @never_used = Computer.never_used
-    status = {
-      in_use: @in_use,
-      not_in_use: @not_in_use,
-      not_pinging: @not_pinging,
-      pinging: @pinging,
-      staying_active: @staying_active,
-      not_staying_active: @not_staying_active
-    }
-    respond_to do |format|
-      format.html
-      format.json { render :json => status }
-    end
+    @locations = Location.all
   end
 
   def by_status
     @status_name = params[:status].humanize
-    
+
     case params[:status]
     when "in_use"
       @computers = Computer.in_use
